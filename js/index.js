51588534108b3af4eaeeff1309148f8250f83be7
aabbbbbcc <aabbbbbcc@163.com>
2019-11-01 17:53:43 +0800

    添加图片和样式

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 var oBox=document.querySelector('.box')
 oBox.onclick=function(){
-	console.log(666)
+	oBox.style.background='yellow'
+}
+setInterval(function(){
+	oBox.style.background=randomColor()
+},500)
+
+function randomColor(){
+    var str = '0123456789abcdef';//0-15
+    var col = '#';
+    for(var i = 0; i < 6; i++){
+        var num = Math.floor(Math.random()*16);
+        col += str[num];
+    }
+    return col;
 }
